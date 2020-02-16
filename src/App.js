9ed251782c5c2a156011fdc3e6160ce36f56d2d4
Alexander <hello@alexander.works>
2020-02-17 00:07:13 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div className="main-wrapper">
+    <div className='main-wrapper'>
       <h1>Level Up Dishes</h1>
+      <input type='text' />
     </div>
   );
 };
