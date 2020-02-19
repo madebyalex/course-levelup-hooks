c7cf8a59cbed10e980f33da22c60a8077c8213a5
Alexander <hello@alexander.works>
2020-02-19 23:48:56 +0200

    Fetch and display data from API

--- src/App.js
+++ src/App.js
@@ -5,22 +5,28 @@ import { useDocumentsTitle } from './hooks/useDocumentsTitle';
 const App = () => {
   const [name, setName] = useDocumentsTitle('');
   const submitBtn = useRef();
+  const [dishes, setDishes] = useState([]);
 
   function handleChange(e) {
     setName(e.target.value);
   }
 
+  const fetchDishes = async () => {
+    const res = await fetch(
+      'https://my-json-server.typicode.com/leveluptuts/fakeapi/dishes'
+    );
+    const data = await res.json();
+    setDishes(data);
+    // console.log(data);
+  };
+
+  useEffect(() => {
+    fetchDishes();
+  }, []);
+
   return (
     <div className='main-wrapper'>
-      <h1
-        onClick={() => {
-          submitBtn.current.classList.contains('new-fake-class')
-            ? submitBtn.current.classList.remove('new-fake-class')
-            : submitBtn.current.classList.add('new-fake-class');
-        }}
-      >
-        Level Up Dishes
-      </h1>
+      <h1>Level Up Dishes</h1>
 
       <Toggle />
 
@@ -45,6 +51,18 @@ const App = () => {
           Submit
         </button>
       </form>
+
+      {dishes.map(dish => (
+        <article key={dish.name} className='dish-card dish-card--withImage'>
+          <h3>{dish.name}</h3>
+          <p>{dish.desc}</p>
+          <div className='ingredients'>
+            {dish.ingredients.map(ingredient => (
+              <span key={ingredient}>{ingredient}</span>
+            ))}
+          </div>
+        </article>
+      ))}
     </div>
   );
 };
