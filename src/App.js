aba8769ca8b114cd9b2cd04b5cfae57c89b1625b
Alexander <hello@alexander.works>
2020-02-20 03:25:34 +0200

    Using react-spring: Simple fade-in animation

--- src/App.js
+++ src/App.js
@@ -1,67 +1,52 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import useAbortableFetch from 'use-abortable-fetch';
+import { useSpring, animated } from 'react-spring';
 import Toggle from './components/Toggle';
 import { useDocumentsTitle } from './hooks/useDocumentsTitle';
 
 const App = () => {
   const [name, setName] = useDocumentsTitle('');
-  const submitBtn = useRef();
+  const ref = useRef();
 
-  const { data = [], loading } = useAbortableFetch(
+  const { data, loading } = useAbortableFetch(
     'https://my-json-server.typicode.com/leveluptuts/fakeapi/dishes'
   );
 
-  // console.log('data: ', data);
-  console.log(loading);
-  if (loading) {
-    document.title = 'Loading';
-  }
-
-  if (!data) return null;
-
-  function handleChange(e) {
-    setName(e.target.value);
-  }
-
+  const props = useSpring({ opacity: 1, from: { opacity: 0 } });
   return (
-    <div className='main-wrapper'>
-      <h1>Level Up Dishes</h1>
-
+    <div className='main-wrapper' ref={ref}>
+      <animated.h1
+        style={props}
+        onClick={() => ref.current.classList.add('new-fake-class')}
+      >
+        Level Up Dishes
+      </animated.h1>
       <Toggle />
-
-      <br />
-      <br />
-
-      {name && (
-        <p>
-          <strong>Orders for: </strong>
-          {name}
-        </p>
-      )}
 
       <form
         onSubmit={e => {
           e.preventDefault();
         }}
       >
-        <input type='text' onChange={handleChange} value={name} />
-
-        <button className='btn-submit' ref={submitBtn} type='submit'>
-          Submit
-        </button>
+        <input
+          type='text'
+          onChange={e => setName(e.target.value)}
+          value={name}
+        />
+        <button>Submit</button>
       </form>
-
-      {data.map(dish => (
-        <article key={dish.name} className='dish-card dish-card--withImage'>
-          <h3>{dish.name}</h3>
-          <p>{dish.desc}</p>
-          <div className='ingredients'>
-            {dish.ingredients.map(ingredient => (
-              <span key={ingredient}>{ingredient}</span>
-            ))}
-          </div>
-        </article>
-      ))}
+      {data &&
+        data.map(dish => (
+          <article className='dish-card dish-card--withImage'>
+            <h3>{dish.name}</h3>
+            <p>{dish.desc}</p>
+            <div className='ingredients'>
+              {dish.ingredients.map(ingredient => (
+                <span>{ingredient}</span>
+              ))}
+            </div>
+          </article>
+        ))}
     </div>
   );
 };
